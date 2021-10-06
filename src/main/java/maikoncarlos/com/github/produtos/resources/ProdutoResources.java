7b5f71c885ae43e61edc1ce3877456bf7d9625d5
Maikoncarlos <maikonsaturno@gmail.com>
2021-10-05 23:44:56 -0300

    add pacote service para gerenciar a comunicação entre as entidades

--- src/main/java/maikoncarlos/com/github/produtos/resources/ProdutoResources.java
+++ src/main/java/maikoncarlos/com/github/produtos/resources/ProdutoResources.java
@@ -3,11 +3,10 @@ package maikoncarlos.com.github.produtos.resources;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import maikoncarlos.com.github.produtos.models.Produto;
-import maikoncarlos.com.github.produtos.repository.ProdutoRepository;
+import maikoncarlos.com.github.produtos.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,35 +17,38 @@ import java.util.List;
 public class ProdutoResources {
 
     @Autowired
-    ProdutoRepository produtoRepository;
+    ProdutoService service;
 
     @GetMapping("/produtos")
     @ApiOperation(value = "Retorna uma lista de produtos")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public List<Produto> listaProdutos() {
-        return produtoRepository.findAll();
+        return service.getProdutos();
     }
 
     @GetMapping("/produto/{id}")
     @ApiOperation(value = "Retorna um produto especifico")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public Produto listaUmProduto(@PathVariable Long id) {
-        return produtoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não Encontrado"));
+        return service.getProdutoId(id);
     }
 
     @PostMapping("/produto")
     @ApiOperation(value = "Salva um produto")
+    @ResponseStatus(HttpStatus.CREATED)
     public Produto salvaProduto(@RequestBody Produto produto) {
-        return produtoRepository.save(produto);
+        return service.novoProduto(produto);
     }
 
-    @PutMapping("/produto")
+    @PutMapping("/produto/{id}")
     @ApiOperation(value = "Atualiza um produto")
-    public Produto atualizaProduto(@RequestBody Produto produto) {
-        return produtoRepository.save(produto);
+    public void atualizaProduto(@PathVariable Long id, @RequestBody Produto produto) {
+         service.atualizaProduto(id, produto);
     }
 
-    @PostMapping("/produto-delete")
+    @PostMapping("/produto-delete/{id}")
     @ApiOperation(value = "Deleta um produto")
-    public void deletaProduto(@RequestBody Produto produto) {
-        produtoRepository.delete(produto);
+    public void deletaProduto(@PathVariable Long id ) {
+        service.excluirProduto(id);
     }
 }
--- src/main/java/maikoncarlos/com/github/produtos/service/ProdutoService.java
+++ src/main/java/maikoncarlos/com/github/produtos/service/ProdutoService.java
@@ -1,4 +1,51 @@
 package maikoncarlos.com.github.produtos.service;
 
+import maikoncarlos.com.github.produtos.models.Produto;
+import maikoncarlos.com.github.produtos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    public List<Produto> getProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto getProdutoId(Long id) {
+        return produtoRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+
+    public Produto novoProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void atualizaProduto(Long id, Produto produtoatualizado) {
+        produtoRepository.findById(id).map(produto -> {
+                    produto.setNome(produtoatualizado.getNome());
+                    produto.setQuantidade(produtoatualizado.getQuantidade());
+                    produto.setValor(produtoatualizado.getValor());
+                    return produtoRepository.save(produto);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+
+    public void excluirProduto(Long id) {
+        produtoRepository.findById(id).map(produto -> {
+                    produtoRepository.delete(produto);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+
+    }
+
 }
+
